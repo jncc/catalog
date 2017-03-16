fe4c11a127d398d8df8963c93df4199a0b0afdb6
felix <felix.mason@jncc.gov.uk>
2017-03-16 15:56:35 +0000

    Better buld stuff

--- api/server.ts
+++ api/server.ts
@@ -12,12 +12,11 @@ process.on('unhandledRejection', r => console.log(r));
 // parse json body requests
 app.use(bodyParser.json());
 
-app.get('/test', async (req, res) => {
-  res.send("you got me")
+app.get(`/test`, async (req, res) => {
+  res.send('donkey')
 })
 
-// custom wms GetCapabilites handler
-app.get(`/search:term`, async (req, res) => {
+app.get(`/searchthings/:testy`, async (req, res) => {
 
 //   let storedQuery = await storedQueryRepository.load(req.params.key)
 //   let queryResult = getProducts(storedQuery.query);
@@ -28,13 +27,33 @@ app.get(`/search:term`, async (req, res) => {
 //   let realWmsUrl = getRealWmsUrl(app.settings.env, req.header(`Host`), req.protocol);
 //   let result = getCapabilities(products, realWmsUrl);
 
-  let result = req.params.term
+  //let result = req.params.term
   //res.set(`Content-Type`, `text/xml`);
 
   res.json({
-    "term" : req.params.term
+    "term" : req.params.testy
   });
 });
+
+// custom wms GetCapabilites handler
+// app.get(`/search:term`, async (req, res) => {
+
+// //   let storedQuery = await storedQueryRepository.load(req.params.key)
+// //   let queryResult = getProducts(storedQuery.query);
+
+// //   // get all the products out of the query result, ignoring the collection they're in
+// //   let products = queryResult.collections.map(c => c.products).reduce((a, b) => a.concat(b));
+
+// //   let realWmsUrl = getRealWmsUrl(app.settings.env, req.header(`Host`), req.protocol);
+// //   let result = getCapabilities(products, realWmsUrl);
+
+//   let result = req.params.term
+//   //res.set(`Content-Type`, `text/xml`);
+
+//   res.json({
+//     "term" : req.params.term
+//   });
+// });
 
 // return product metadata to the ui
 // app.get(`/products`, (req, res) => {
