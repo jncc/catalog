797e3702fdb39e22a6ec7200f71e312a169eb5a3
felix <felix.mason@jncc.gov.uk>
2017-03-30 12:31:09 +0100

    geojson validation

--- api/product/validateProduct.ts
+++ api/product/validateProduct.ts
@@ -1,46 +1,64 @@
 import { Product } from "./product";
+//import { GeoJSON } from "geojson";
+
+//geoJson hint without json validation.
+import geojsonhint = require("@mapbox/geojsonhint/lib/object")
+
+
+function validateFootprint(f, errors:string[]){
+
+    let result = geojsonhint.hint(f,{
+        precisionWarning:false
+    });
+
+    result.forEach(e => {
+        errors.push("footprint: " + e.message)
+    });
+    //TODO check valid CRS definition. EPSG:4326
+
+}
 
 /* Throws an exception if the query isn't good. */
-export function validateProduct(p: Product) {
+export function validateProduct(p: Product): string[] {
+    let errors:string[] = []
+
     //collection name
-        //defined
+    if (!p.collectionName || p.collectionName.length === 0) {
+        errors.push("collectionName not specified");
+    }
         //exists
+
     //name 
-        //defined
+    if (!p.name || p.name.length === 0) {
+        errors.push("name not specified");
+    }
         //is unique
     //metadata
-        //defined
+    if (!p.metadata) {
+        errors.push("metadata not specified");
+    }
         //TODO: pinch petes metadata validation logic from TC
     //footprint
-        //defined
+    if (!p.footprint) {
+        errors.push("footprint not defined");
+    }
+    else {
+        validateFootprint(p.footprint, errors);
+    }
+
         //valid geojason
         //4326 CRS defined
         //multipolygon
     //properties
-        //defined
-    //data
-        //defined
-        //has at least one valid entry
-        
-    if (!p.name || p.name.length === 0) {
-        throw "Product name not specified";
+    if (!p.properties) {
+        errors.push("properties not specified");
     }
+        //defined
 
-    // if (!o.collections || o.collections.length === 0) {
-    //     throw "Query validation failed. No collection specified.";
-    // }
-
-    // if (!o.bbox) {
-    //     throw "Query validation failed. No bbox specified.";
-    // }
-
-    // if (!Array.isArray(o.bbox)) {
-    //     throw "Query validation failed. Bbox not array.";
-    // }
-
-    // if (o.bbox.length !== 4) {
-    //     throw "Query validation failed. Bbox wrong length.";
-    // }
-
-    // todo.... more validations
+    //data  
+    if (!p.data) {
+        errors.push("data not specified");
+    }
+        //has at least one valid entry
+    return errors;
 }
