99091987460962c6b56c0109b565bc8b032b583a
Matt Debont <matt.debont@jncc.gov.uk>
2019-02-19 11:14:31 +0000

    Added some additional config around the logger, made the bundle/tail ops look at /var/log/catalog/catalog.log

--- api/logging/logger.ts
+++ api/logging/logger.ts
@@ -9,7 +9,7 @@ export class Logger {
         level: process.env.LOG_LEVEL === undefined ? 'info' : process.env.LOG_LEVEL,
         transports: [
           new winston.transports.File({
-            filename: './logs/combined.log',
+            filename: process.env.LOG_LOCATION === undefined ? './logs/stdout.log' : process.env.LOG_LOCATION,
             format: winston.format.combine(
               winston.format.timestamp(),
               winston.format.json()
@@ -22,6 +22,9 @@ export class Logger {
     if (process.env.NODE_ENV !== 'production') {
       this.singletonLogger.add(new winston.transports.Console({format: winston.format.simple()}));
     }
+
+    this.singletonLogger.info(`Log level is ${process.env.LOG_LEVEL}`)
+    this.singletonLogger.info(`Log Location is ${process.env.LOG_LOCATION}`)
 
     return this.singletonLogger;
   }
