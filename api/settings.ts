25f2737d684d4b55dd3b45f2a0d9890fd0f77e77
Matt Debont <matt.debont@jncc.gov.uk>
2019-02-21 15:15:57 +0000

    Removing dev time port selection as its not needed here really

--- api/settings.ts
+++ api/settings.ts
@@ -3,19 +3,10 @@ import * as dotenv from 'dotenv';
 export function getEnvironmentSettings(env: string) {
   dotenv.config();
 
-  if (env === "development") {
-    return {
-      name: env,
-      dev: true,
-      port: 8888,
-      dir: ""
-    };
-  } else {
-    return {
-      name: env,
-      dev: false,
-      port: 8081,  // elastic beanstalk: the default nginx configuration forwards traffic to an upstream server named nodejs at 127.0.0.1:8081
-      dir: 'built'
-    };
+  return {
+    name: env,
+    dev: false,
+    port: 8081,  // elastic beanstalk: the default nginx configuration forwards traffic to an upstream server named nodejs at 127.0.0.1:8081
+    dir: 'built'
   }
 }
